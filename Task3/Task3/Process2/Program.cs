661d8ce2902aaad0823a883a2831cf54eaeaab19
MeanLesss <68323176+MeanLesss@users.noreply.github.com>
2022-06-15 17:06:33 +0700

    ads

--- Task3/Task3/Process2/Program.cs
+++ Task3/Task3/Process2/Program.cs
@@ -1,22 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Task3
+namespace Process2
 {
     internal class Program
     {
-        public static List<int> OddNumber = new List<int>();
+        public static List<int> EvenNumber = new List<int>();
 
         static void Main(string[] args)
         {
-
-            Mutex mutex = new Mutex();
-
             Thread[] threads = new Thread[1];
             for (int i = 0; i < threads.Length; ++i)
             {
@@ -27,40 +23,31 @@ namespace Task3
             for (int i = 0; i < threads.Length; ++i)
                 threads[i].Join();
 
-            Console.WriteLine("Process 1:");
-            foreach (var odd in OddNumber)
+            Console.WriteLine("Process 2:");
+            foreach (var Even in EvenNumber)
             {
-                Console.WriteLine(odd);
+                Console.WriteLine(Even);
             }
-
-            mutex.WaitOne();
-            Process process = new Process();
-            process.StartInfo.FileName = "Process2.exe";
-            process.Start();
-            process.WaitForExit();
-            mutex.ReleaseMutex();
-
-
             Console.Read();
-        }
 
+        }
         int count;
 
         public static void UpdateFields()
         {
             for (int i = 1; i <= 10; i++)
             {
-                if (i % 2 != 0)
+                if (i % 2 == 0)
                 {
-                    OddNumber.Add(i);
+                    EvenNumber.Add(i);
                 }
             }
 
-            for (int i = 10; i > 0; i--)
+            for (int i = 9; i > 0; i--)
             {
-                if (i % 2 != 0)
+                if (i % 2 == 0)
                 {
-                    OddNumber.Add(i);
+                    EvenNumber.Add(i);
                 }
             }
         }
